3e62e9afcfa42be01cf47368202f23eec3a1cb85
tpetry <github@tpetry.me>
2024-07-31 11:01:07 +0200

    wip

--- src/Schema/Grammars/GrammarTable.php
+++ src/Schema/Grammars/GrammarTable.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Tpetry\PostgresqlEnhanced\Schema\Grammars;
 
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Arr;
+use Arr;
 use Illuminate\Support\Fluent;
 
 trait GrammarTable
